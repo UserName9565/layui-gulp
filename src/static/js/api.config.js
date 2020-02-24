ad261ccdcdbe6d04c96ff51882ff147399e9ac63
zhangxin@agilestar.cn <zhangxin@agilestar.cn>
2020-02-24 15:55:02 +0800

    修改为本地地址，进行打包测试

--- src/static/js/api.config.js
+++ src/static/js/api.config.js
@@ -2,9 +2,9 @@
 var apiConfig = {
   // ctx: "http://192.168.1.79:8080",
  
-  ctx: "http://192.168.1.152:9600/sysmgr",//接口地址http://192.168.1.76:8090/ api-gateway
+  ctx: "http://localhost:9600/sysmgr",//接口地址http://192.168.1.76:8090/ api-gateway
   // ctx: "http://192.168.1.76:9600/sysmgr",
-  oldVersion:"http://192.168.1.152:8090"//嵌入页面工程地址
+  oldVersion:"http://localhost:8090"//嵌入页面工程地址
    
 }
 
