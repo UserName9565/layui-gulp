1298641df29ad380e4ffb914574c708779f7b592
FengtianBAO <baofengtian@agilestar.cn>
2020-03-04 14:29:15 +0800

    测试打包的路径改为/

--- config/api.config.test.js
+++ config/api.config.test.js
@@ -5,6 +5,6 @@ var apiConfig = {
   ctx: "http://bhps.agilestar.cn:9600",//接口地址http://192.168.1.76:8090/ api-gateway
   // ctx: "http://192.168.1.76:9600/sysmgr",
   oldVersion:"http://bhps.agilestar.cn",//嵌入页面工程地址
-  rootD:"/bhps" //根目录前置路径
+  rootD:"/" //根目录前置路径
 }
 
