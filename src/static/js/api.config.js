8d6ca79d50860815de4e26959b53f9689bab2102
jiangyong@agilestar.cn <jiangyong@agilestar.cn>
2020-03-16 17:57:28 +0800

    daima tijiao

--- src/static/js/api.config.js
+++ src/static/js/api.config.js
@@ -2,9 +2,9 @@
 var apiConfig = {
   // ctx: "http://192.168.1.79:8080",
  
-  ctx: "http://PC-20200107QQTG:9600",//接口地址http://192.168.1.76:8090/ api-gateway
+  ctx: "http://bhps.agilestar.cn:9600",//接口地址http://192.168.1.76:8090/ api-gateway
   // ctx: "http://192.168.1.76:9600/sysmgr",
-  oldVersion:"http://PC-20200107QQTG",//嵌入页面工程地址
+  oldVersion:"http://bhps.agilestar.cn",//嵌入页面工程地址
   rootD:""//根目录前置路径
    
 }
