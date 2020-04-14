058dc952644b7c9234974c59e0e0f55703b8164d
hanxiangyun <hanxiangyun@agilestar.cn>
2020-04-14 09:01:33 +0800

    api.config

--- src/static/js/api.config.js
+++ src/static/js/api.config.js
@@ -1,6 +1,6 @@
 // api config
 var apiConfig = {
-   //ctx: "http://localhost:9600",
+  // ctx: "http://localhost:9600",
  
   ctx: "http://bhps.agilestar.cn:9600",//接口地址http://192.168.1.76:8090/ api-gateway
   // ctx: "http://192.168.1.76:9600/sysmgr",
